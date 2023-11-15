4399ba9f9c9a3e5497bf2966ee004732c664e1a8
hong9Lee <hongtemp9mac@gmail.com>
2023-11-15 21:09:16 +0900

    set 자료구조를 활용하여 1인당 1개의 쿠폰만 발급

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.example.api.service;
 
 import com.example.api.domain.Coupon;
 import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,24 @@ public class ApplyService {
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    private final AppliedUserRepository appliedUserRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponCountRepository.increment();
+        Long apply = appliedUserRepository.add(userId);
 
+        if(apply != 1) {
+            return;
+        }
+
+
+        long count = couponCountRepository.increment();
         if(count > 1000) {
             return;
         }
